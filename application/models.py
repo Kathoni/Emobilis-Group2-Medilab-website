5b3a53f32813b67c6e7bca313448a3225ebcafdc
Kathoni <thrusherkeys@gmail.com>
2024-11-16 16:45:53 +0300

    fields update

--- application/models.py
+++ application/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Appointment(models.Model):
+    name = models.CharField(max_length=20)
+    gender = models.CharField(max_length=20)
+    date = models.DateField()
+    time = models.TimeField()
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
